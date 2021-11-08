a162174f96aa97725cc36c330d0db2ea235a9a63
David Zapata <dzg00@iesemilidarder.com>
2021-11-08 20:44:52 +0100

    v

--- 1AVA/IAW_EX1-zapata/js/app.js
+++ 1AVA/IAW_EX1-zapata/js/app.js
@@ -1,32 +1,51 @@
 const containerTravels = document.querySelector("#container-travels");
 let travelsJSON = [];
+let favsUser = [];
 
 
 
 document.addEventListener("DOMContentLoaded", () => {
     getTravels();
+    createLocalData();
+    addLocalData();
     
 });
 
-function getTravels() {
+
+const getTravels = () =>{
     fetch('data/travels.json')
-        .then(result => result.json())
-        .then(data => {
-            travelsJSON = data;
-            populateTravelsHTML(travelsJSON);
-            console.log(travelsJSON)
-        });
+    .then(result => result.json())
+    .then(data => {
+        travelsJSON = data;
+        populateTravelsHTML(travelsJSON);
+    });
 }
 
-function populateTravelsHTML(travelsJSON) {
+const populateTravelsHTML = (travelsJSON) =>{
     travelsJSON.forEach(travel => {
         // Generate card question
         containerTravels.innerHTML +=
             `<article class="entrada">
-            <h2><img id="post-1" class="favorite" src="img/favorite-off-icon.png" alt="marcar como favorita" />${travel.title}</h2>
+            <h2><img id="post-1" class="favorite" src="img/favorite-${putStars(travel.id)}-icon.png" />${travel.title}</h2>
             <img src="${travel.img}" alt="Imagen Viaje a Londres">
             <p>${travel.description}</p>
             <a href="#" class="boton">Leer Más</a>
           </article>`;
     });
+}
+
+const createLocalData = () =>{
+    localStorage.setItem('user', JSON.stringify((favsUser)));
+}
+
+const addLocalData = () =>{
+    let prueba = this.value;
+    console.log(prueba)
+
+    
+    
+}
+
+const putStars = (id) =>{
+
 }
---
+++ 1AVA/poo/js/app.js
@@ -0,0 +1,11 @@
+
+    function Persona(nombre){
+        this.nombre = nombre;
+
+        this.saludar = function(){
+            return `${this.nombre} dice hola`
+        }
+    }
+
+    const david = new Persona("david")
+    console.log(david.saludar());
